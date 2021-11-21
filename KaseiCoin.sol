6e73cd763b8eb73f38b4568d3d82761b460ad43c
Julia Milin <juliamilin@yahoo.com>
2021-11-21 17:40:09 -0500

    adding final code

--- KaseiCoin.sol
+++ KaseiCoin.sol
@@ -5,19 +5,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
 contract KaseiCoin is ERC20, ERC20Detailed, ERC20Mintable {
-    //address payable owner;
+    
+    constructor(string memory name, string memory symbol, uint initial_supply) ERC20Detailed(name, symbol, 18) public {}
 
-    //modifier onlyOwner {
-        //require(msg.sender == owner, "You do not have permission to mint these tokens!");
-       // _;
-    //}
-
-    constructor(string memory name, string memory symbol, uint initial_supply) ERC20Detailed(name, symbol, 18) public {
-        //owner = msg.sender;
-        //_mint(owner, initial_supply);
-    }
-
-    //function mint(address recipient, uint amount) public onlyOwner {
-       // _mint(recipient, amount);
-    //}
 }
